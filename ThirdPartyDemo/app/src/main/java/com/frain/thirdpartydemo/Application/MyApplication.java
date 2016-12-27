f2c483c2e32eb0d0342d78bc758533a7376fe405
furuiCQ <1182590636@qq.com>
2016-12-27 09:23:23 +0800

    第一次提交目录结构

---
+++ ThirdPartyDemo/app/src/main/java/com/frain/thirdpartydemo/Application/MyApplication.java
@@ -0,0 +1,14 @@
+package com.frain.thirdpartydemo.Application;
+
+import android.app.Application;
+
+/**
+ * Created by admin on 2016/12/27.
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
